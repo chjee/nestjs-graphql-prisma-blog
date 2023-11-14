096b3ead5d82d366b505865a46f3de8fc063edb0
Changhoon-Jee <chjee71@gmail.com>
2023-11-14 18:15:26 +0900

    apply graphql & user modules.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { PostsModule } from './posts/posts.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -8,6 +12,13 @@ import { ConfigModule } from '@nestjs/config';
       cache: true,
       envFilePath: '.env.dev, .env',
     }),
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      autoSchemaFile: true,
+      sortSchema: true,
+    }),
+    PostsModule,
+    UsersModule,
   ],
   providers: [],
 })
