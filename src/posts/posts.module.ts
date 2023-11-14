0d6d3b3f53be1d38d34395b97e8261cd73fbfeeb
Changhoon-Jee <chjee71@gmail.com>
2023-11-14 18:36:11 +0900

    add prisma module.

--- src/posts/posts.module.ts
+++ src/posts/posts.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { PostsResolver } from './posts.resolver';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [PostsResolver, PostsService],
 })
 export class PostsModule {}
