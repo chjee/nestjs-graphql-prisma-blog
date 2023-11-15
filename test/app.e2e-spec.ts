99a8225b73e8eb3fa351418e682f359530bd4345
Changhoon-Jee <chjee71@gmail.com>
2023-11-15 15:35:49 +0900

    add test desc & functions.

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,10 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { HttpStatus, INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+
+  const mockUser = {
+    id: 1,
+    name: 'Andrew',
+    email: 'andrew@prisma.io',
+    role: 'ADMIN',
+  };
+
+  const mockPost = {
+    id: 1,
+    title: 'Just 5 minutes.',
+    published: false,
+    userId: 1,
+  };
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -12,13 +26,220 @@ describe('AppController (e2e)', () => {
     }).compile();
 
     app = moduleFixture.createNestApplication();
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
+    app.enableShutdownHooks();
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  describe('UsersResolver (e2e)', () => {
+    it('createUser', async () => {
+      return await request(app.getHttpServer())
+        .post('/graphql')
+        .send({
+          query: `
+            mutation {
+              createUser(createUserInput: {
+                email: "andrew@prisma.io"
+                name: "Andrew"
+                password: "whoami"
+                role: "ADMIN"
+              }) {
+                id
+                name
+                email
+                role
+              }
+            }
+          `,
+        })
+        .expect(HttpStatus.OK)
+        .expect((res) => {
+          mockUser.id = res.body.id;
+        });
+    });
+
+    it('findAll', async () => {
+      return await request(app.getHttpServer())
+        .post('/graphql')
+        .send({
+          query: `
+            query {
+              users(skip:0, take:3) {
+                id
+                name
+                email
+                role
+              }
+            }
+          `,
+        })
+        .expect(HttpStatus.OK);
+    });
+
+    it('findOne', async () => {
+      return await request(app.getHttpServer())
+        .post('/graphql')
+        .send({
+          query: `
+            query {
+              user(id: ${mockUser.id}) {
+                id
+                name
+                email
+                role
+              }
+            }
+          `,
+        })
+        .expect(HttpStatus.OK);
+    });
+
+    it('updateUser', async () => {
+      return await request(app.getHttpServer())
+        .post('/graphql')
+        .send({
+          query: `
+            mutation {
+              updateUser(id: ${mockUser.id}, 
+              updateUserInput: { name: "Andy", role: "USER" }) {
+                id
+                name
+                email
+                role
+              }
+            }
+          `,
+        })
+        .expect(HttpStatus.OK);
+    });
+
+    it('removeUser', async () => {
+      return await request(app.getHttpServer())
+        .post('/graphql')
+        .send({
+          query: `
+            mutation {
+              removeUser(id: ${mockUser.id}) {
+                id
+                name
+                email
+                role
+              }
+            }
+          `,
+        })
+        .expect(HttpStatus.OK);
+    });
+  });
+  describe('PostsResolver (e2e)', () => {
+    it('createPost', async () => {
+      return await request(app.getHttpServer())
+        .post('/graphql')
+        .send({
+          query: `
+            mutation {
+              createPost(
+                createPostInput: {
+                  title: "Just 5 minutes."
+                  published: false
+                  userId: 1
+                }
+              ) {
+                id
+                title
+                published
+                userId
+              }
+            }
+          `,
+        })
+        .expect(HttpStatus.OK)
+        .expect((res) => {
+          mockPost.id = res.body.id;
+        });
+    });
+
+    it('findAll', async () => {
+      return await request(app.getHttpServer())
+        .post('/graphql')
+        .send({
+          query: `
+            query {
+              posts(skip: 0, take: 3) {
+                id
+                title
+                published
+                userId
+              }
+            }
+          `,
+        })
+        .expect(HttpStatus.OK);
+    });
+
+    it('findOne', async () => {
+      return await request(app.getHttpServer())
+        .post('/graphql')
+        .send({
+          query: `
+            query {
+              post(id: ${mockPost.id}) {
+                id
+                title
+                published
+                userId
+              }
+            }
+          `,
+        })
+        .expect(HttpStatus.OK);
+    });
+
+    it('updatePost', async () => {
+      return await request(app.getHttpServer())
+        .post('/graphql')
+        .send({
+          query: `
+            mutation {
+              updatePost(
+                id: ${mockPost.id},
+                updatePostInput: {
+                  title: "Just 10 minutes.",
+                  published: true }) {
+                id
+                title
+                published
+                userId
+              }
+            }
+          `,
+        })
+        .expect(HttpStatus.OK);
+    });
+
+    it('removePost', async () => {
+      return await request(app.getHttpServer())
+        .post('/graphql')
+        .send({
+          query: `
+            mutation {
+              removePost(id: ${mockPost.id})
+              {
+                id
+                title
+                published
+                userId
+              }
+            }
+          `,
+        })
+        .expect(HttpStatus.OK);
+    });
   });
 });
