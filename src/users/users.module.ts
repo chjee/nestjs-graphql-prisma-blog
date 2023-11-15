c60a377b6e43bd1507a0f1217c0bcf65763e371d
Changhoon-Jee <chjee71@gmail.com>
2023-11-15 12:35:19 +0900

    fix import path.

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersResolver } from './users.resolver';
-import { PrismaModule } from 'src/prisma/prisma.module';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
   imports: [PrismaModule],
