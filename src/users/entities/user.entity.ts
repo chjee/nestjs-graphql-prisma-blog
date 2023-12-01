2c71c49491310570f0e9683ccda18e9f8afbe116
Changhoon-Jee <chjee71@gmail.com>
2023-12-01 13:53:45 +0900

    remove validating decorators.

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,36 +1,25 @@
 import { ObjectType, Field, Int } from '@nestjs/graphql';
 import { Profile } from '../../profiles/entities/profile.entity';
 import { Post } from '../../posts/entities/post.entity';
-import { IsEmail, IsEnum, IsNotEmpty, IsString, Length } from 'class-validator';
 
 @ObjectType({ description: 'User Model' })
 export class User {
   @Field(() => Int, { description: 'User ID' })
-  @IsNotEmpty()
   id: number;
 
   @Field(() => Date, { description: 'Created Date' })
   createdAt: Date;
 
   @Field(() => String, { description: 'User Email' })
-  @IsNotEmpty()
-  @IsEmail()
-  @Length(6, 60)
   email: string;
 
   @Field(() => String, { nullable: true, description: 'User Name' })
   name?: string;
 
   @Field(() => String, { description: 'Password' })
-  @IsNotEmpty()
-  @IsString()
-  @Length(6, 60)
   password: string;
 
   @Field(() => String, { description: 'User Role' })
-  @IsNotEmpty()
-  @IsString()
-  @IsEnum(['ADMIN', 'USER'])
   role: string;
 
   @Field(() => [Post], { nullable: true, description: 'User Posts' })
