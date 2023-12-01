d7bcba8c7daa3ec3358345eefcea9548c2eb181d
Changhoon-Jee <chjee71@gmail.com>
2023-12-01 21:00:22 +0900

    rename & move to common dir.

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,7 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthResolver } from './auth.resolver';
 import { JwtModule } from '@nestjs/jwt';
-import { jwtConstants } from '../common/guards/constants';
+import { jwtConstants } from '../common/constants/jwt.constants';
 import { JwtStrategy } from '../common/guards/jwt.strategy';
 import { UsersModule } from '../users/users.module';
 
