0d94d6b5876344574e870cc3d90744f91ae7a7d8
Changhoon-Jee <chjee71@gmail.com>
2023-12-02 10:34:59 +0900

    add jest functions to resolvefield.

--- src/users/users.resolver.spec.ts
+++ src/users/users.resolver.spec.ts
@@ -6,14 +6,18 @@ import {
   users,
   createUserInput,
   updateUserInput,
+  post,
+  profile,
 } from '../common/constants/jest.constants';
 import { PrismaService } from '../prisma/prisma.service';
 import { PostsService } from '../posts/posts.service';
 import { ProfilesService } from '../profiles/profiles.service';
 
 describe('UsersResolver', () => {
-  let resolver: UsersResolver;
-  let service: UsersService;
+  let usersResolver: UsersResolver;
+  let usersService: UsersService;
+  let postsService: PostsService;
+  let profilesService: ProfilesService;
 
   beforeEach(async () => {
     const moduleRef: TestingModule = await Test.createTestingModule({
@@ -26,42 +30,62 @@ describe('UsersResolver', () => {
       ],
     }).compile();
 
-    resolver = moduleRef.get<UsersResolver>(UsersResolver);
-    service = moduleRef.get<UsersService>(UsersService);
+    usersResolver = moduleRef.get<UsersResolver>(UsersResolver);
+    usersService = moduleRef.get<UsersService>(UsersService);
+    postsService = moduleRef.get<PostsService>(PostsService);
+    profilesService = moduleRef.get<ProfilesService>(ProfilesService);
   });
 
   describe('create', () => {
     it('should return a user', async () => {
-      jest.spyOn(service, 'create').mockImplementation(async () => user);
-      expect(await resolver.createUser(createUserInput)).toBe(user);
+      jest.spyOn(usersService, 'create').mockImplementation(async () => user);
+      expect(await usersResolver.createUser(createUserInput)).toBe(user);
     });
   });
 
   describe('findAll', () => {
     it('should return an array of users', async () => {
-      jest.spyOn(service, 'findAll').mockImplementation(async () => users);
-      expect(await resolver.findAll(0, 2)).toBe(users);
+      jest.spyOn(usersService, 'findAll').mockImplementation(async () => users);
+      expect(await usersResolver.findAll(0, 2)).toBe(users);
     });
   });
 
   describe('findOne', () => {
     it('should return a user', async () => {
-      jest.spyOn(service, 'findOne').mockImplementation(async () => user);
-      expect(await resolver.findOne(1)).toBe(user);
+      jest.spyOn(usersService, 'findOne').mockImplementation(async () => user);
+      expect(await usersResolver.findOne(1)).toBe(user);
+    });
+  });
+
+  describe('posts', () => {
+    it('should return an array of posts', async () => {
+      jest
+        .spyOn(postsService, 'findAll')
+        .mockImplementation(async () => [post]);
+      expect(await usersResolver.posts(user)).toEqual([post]);
+    });
+  });
+
+  describe('profile', () => {
+    it('should return a profile', async () => {
+      jest
+        .spyOn(profilesService, 'findOne')
+        .mockImplementation(async () => profile);
+      expect(await usersResolver.profile(user)).toBe(profile);
     });
   });
 
   describe('update', () => {
     it('should return a user', async () => {
-      jest.spyOn(service, 'update').mockImplementation(async () => user);
-      expect(await resolver.updateUser(1, updateUserInput)).toBe(user);
+      jest.spyOn(usersService, 'update').mockImplementation(async () => user);
+      expect(await usersResolver.updateUser(1, updateUserInput)).toBe(user);
     });
   });
 
   describe('remove', () => {
     it('should return a user', async () => {
-      jest.spyOn(service, 'remove').mockImplementation(async () => user);
-      expect(await resolver.removeUser(1)).toBe(user);
+      jest.spyOn(usersService, 'remove').mockImplementation(async () => user);
+      expect(await usersResolver.removeUser(1)).toBe(user);
     });
   });
 });
