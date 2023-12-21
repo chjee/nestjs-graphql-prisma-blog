8ef8b8cd71f461ce1353542c509ab94f2d58623e
Changhoon-Jee <chjee71@gmail.com>
2023-12-21 18:10:52 +0900

    add test functions accoring to adding resolvefields.

--- src/posts/posts.resolver.spec.ts
+++ src/posts/posts.resolver.spec.ts
@@ -2,24 +2,38 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { PostsResolver } from './posts.resolver';
 import { PostsService } from './posts.service';
 import { PrismaService } from '../prisma/prisma.service';
+import { UsersService } from './../users/users.service';
+import { CategoriesService } from './../categories/categories.service';
 import {
   post,
   posts,
   createPostInput,
   updatePostInput,
+  user,
+  categories,
 } from '../common/constants/jest.constants';
 
 describe('PostsResolver', () => {
   let postsResolver: PostsResolver;
   let postsService: PostsService;
+  let usersService: UsersService;
+  let categoriesService: CategoriesService;
 
   beforeEach(async () => {
     const moduleRef: TestingModule = await Test.createTestingModule({
-      providers: [PrismaService, PostsResolver, PostsService],
+      providers: [
+        PrismaService,
+        PostsResolver,
+        PostsService,
+        UsersService,
+        CategoriesService,
+      ],
     }).compile();
 
     postsResolver = moduleRef.get<PostsResolver>(PostsResolver);
     postsService = moduleRef.get<PostsService>(PostsService);
+    usersService = moduleRef.get<UsersService>(UsersService);
+    categoriesService = moduleRef.get<CategoriesService>(CategoriesService);
   });
 
   describe('create', () => {
@@ -40,6 +54,22 @@ describe('PostsResolver', () => {
     it('should return a post', async () => {
       jest.spyOn(postsService, 'findOne').mockImplementation(async () => post);
       expect(await postsResolver.findOne(1)).toBe(post);
+    });
+  });
+
+  describe('user', () => {
+    it('should return a user', async () => {
+      jest.spyOn(usersService, 'findOne').mockImplementation(async () => user);
+      expect(await postsResolver.user(post)).toBe(user);
+    });
+  });
+
+  describe('categories', () => {
+    it('should return an array of categories', async () => {
+      jest
+        .spyOn(categoriesService, 'findAll')
+        .mockImplementation(async () => categories);
+      expect(await postsResolver.categories(post)).toBe(categories);
     });
   });
 
