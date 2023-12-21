d8890ddf99a8be0ae446025c9629faac8d84a348
Changhoon-Jee <chjee71@gmail.com>
2023-12-21 19:48:10 +0900

    register category supertest file.

---
+++ test/categories.e2e-spec.ts
@@ -0,0 +1,135 @@
+import * as request from 'supertest';
+import { Test, TestingModule } from '@nestjs/testing';
+import { HttpStatus, INestApplication } from '@nestjs/common';
+import { AppModule } from '../src/app.module';
+import { JwtAuthGuard } from '../src/common/guards/jwt-auth.guard';
+import { CategoriesService } from '../src/categories/categories.service';
+
+describe('CategoriesResolver (e2e)', () => {
+  let app: INestApplication;
+  const categoriesService = {
+    create: () => mockCategory,
+    findAll: () => [mockCategory],
+    findOne: () => mockCategory,
+    update: () => mockCategory,
+    remove: () => mockCategory,
+  };
+
+  const mockCategory = {
+    id: 1,
+    name: 'Database',
+  };
+
+  beforeAll(async () => {
+    const moduleRef: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
+    })
+      .overrideProvider(JwtAuthGuard)
+      .useValue({ canActivate: () => true })
+      .overrideProvider(CategoriesService)
+      .useValue(categoriesService)
+      .compile();
+
+    app = moduleRef.createNestApplication();
+    await app.init();
+  });
+
+  it('createCategory', async () => {
+    return await request(app.getHttpServer())
+      .post('/graphql')
+      .send({
+        query: `
+          mutation {
+            createCategory(createCategoryInput: {
+              name: "Database"
+            })
+            {
+              id
+              name
+            }
+          }
+        `,
+      })
+      .expect(HttpStatus.OK)
+      .expect({ data: { createCategory: categoriesService.create() } });
+  });
+
+  it('findAll', async () => {
+    return await request(app.getHttpServer())
+      .post('/graphql')
+      .send({
+        query: `
+          query {
+            getCategories(skip:0, take:2)
+            {
+              id
+              name
+            }
+          }
+        `,
+      })
+      .expect(HttpStatus.OK)
+      .expect({ data: { getCategories: categoriesService.findAll() } });
+  });
+
+  it('findOne', async () => {
+    return await request(app.getHttpServer())
+      .post('/graphql')
+      .send({
+        query: `
+          query {
+            getCategoryById(id: 1)
+            {
+              id
+              name
+            }
+          }
+        `,
+      })
+      .expect(HttpStatus.OK)
+      .expect({ data: { getCategoryById: categoriesService.findOne() } });
+  });
+
+  it('updateCategory', async () => {
+    return await request(app.getHttpServer())
+      .post('/graphql')
+      .send({
+        query: `
+          mutation {
+            updateCategory(id: 4, 
+            updateCategoryInput: {
+              name: "Science",
+            })
+            {
+              id
+              name
+            }
+          }
+        `,
+      })
+      .expect(HttpStatus.OK)
+      .expect({ data: { updateCategory: categoriesService.update() } });
+  });
+
+  it('removeCategory', async () => {
+    return await request(app.getHttpServer())
+      .post('/graphql')
+      .send({
+        query: `
+          mutation {
+            removeCategory(id: 4)
+            {
+              id
+              name
+            }
+          }
+        `,
+      })
+      .expect(HttpStatus.OK)
+      .expect({ data: { removeCategory: categoriesService.remove() } });
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+});
